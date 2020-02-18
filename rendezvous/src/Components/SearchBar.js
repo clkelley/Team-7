c58f248a7af2d8e3efcc271663bb4e7c90698010
Semir Shafi <semirshafi@10-16-221-103.dynapool.wireless.nyu.edu>
2020-02-18 11:46:54 -0500

    Added querying database to searchbar

--- rendezvous/src/Components/SearchBar.js
+++ rendezvous/src/Components/SearchBar.js
@@ -3,37 +3,43 @@ import './SearchBar.css';
 import FilterCategory from './FilterCategory';
 import Grid from '@material-ui/core/Grid';
 import { MenuIcon, SearchIcon } from '@material-ui/icons/Menu';
-import {Chip} from '@material-ui/core';
+import {Chip} from '@material-ui/core';import Firebase from 'firebase'
+import { db } from '../firebase';
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchTerm: "",
-      matches: [""]
+      matches: {}
     };
-    
+
     this.handleInput = event => this.handleSearch(event);
     this.searchEvents = event => this.search(event);
   }
-  
+
   handleSearch(event) {
   	this.setState({ searchTerm: event.target.value });
  }
- 
+
  search(event) {
- 	
+   db.collection("event_search")
+   .where("category", "==", "u" )
+   .get()
+   .then(querySnapshot => {
+     const data = querySnapshot.docs.map(doc => doc.data());
+     this.setState({matches: data})
+   })
  }
 
   render() {
-  	
     return (
-    	<Grid container spacing={3}>
+    	<Grid container spacing={1}>
       		<Grid item xs={5}><input className="inputBar" onChange={this.handleInput} placeholder="Explore Events"></input></Grid>
       		<Grid item ><button onclick={this.searchEvents} className="searchButton">Search</button></Grid>
       		<Grid item xs={12}><FilterCategory className="filterCategory" /></Grid>
-      		<div> {this.state.matches} </div>
-    	</Grid> 
+          <div> {JSON.stringify(this.state.matches)} </div>
+    	</Grid>
     );
   }
 }
